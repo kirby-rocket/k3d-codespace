3d5fde4c1d475995e90edbbe20e705fe7146a71c
kumailr7 <kumailrizvi70@gamil.com>
2024-01-08 23:06:47 +0530

    Updating K3D Cluster Codespace and Added README.md file

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,20 +1,45 @@
 # .devcontainer/setup.sh
 #!/bin/bash
 
+# Installation of Brew and Kubernetes tools 
+
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/$USER/.bashrc
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+source ~/.bashrc 
+
+echo "✅ brew installed successfully"
+
+brew install derailed/k9s/k9s
+
+sudo apt-get install fzf -y
+
+echo "✅ kubectx, kubens, fzf, homebrew, and k9s installed successfully"
+
 # Start k3d cluster with three nodes
+echo "✅ Intialize the Kuberentes cluster"
+
 k3d cluster create Dragon --servers 2
 
 # Adding Nodes to the cluster 
+
+echo "✅ Adding Nodes to the Kuberentes cluster"
 
 k3d node create Goku --cluster Dragon
 
 k3d node create Beast --cluster Dragon
 
-
 # Adding the config File
+
+echo "✅ Adding the config files of kubernetes"
+
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 # Show the kubectl nodes 
+
+echo "✅ Showing the kubernetes nodes"
+
 kubectl get nodes 
+
---
+++ README.md
@@ -0,0 +1,16 @@
+# Overview
+A Kirby Rocket K3D Playground to practice Kubernetes skills.
+
+## Instructions
+
+1. You will get access to a Linux terminal with `kubectl`, `helm`, `k9s`, `kubectx`, and `kubens` already installed.
+2. A `K3D Muliti Nodes Cluster` will automatically start, this takes about 5 minutes so please be patient!
+3. I've also included a list of aliases to use:
+- alias k="kubectl"
+- alias kga="kubectl get all"
+- alias kgn="kubectl get all --all-namespaces"
+- alias kdel="kubectl delete"
+- alias kd="kubectl describe"
+- alias kg="kubectl get"
+
+### HAPPY KUBERNETES ###
